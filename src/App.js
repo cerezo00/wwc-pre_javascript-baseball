58d26f9411ffbd8cecb2c14eb638cd021eeeeeef
HW-K <rlaql99@gmail.com>
2024-09-03 23:08:07 +0900

    숫자 야구

--- src/App.js
+++ src/App.js
@@ -1,68 +1,69 @@
 import { Random, Console } from '@woowacourse/mission-utils';
+// Random 값 추출은 Random.pickNumberInRange()를 활용한다
+// 사용자의 값을 입력 받고 출력하기 위해서는 Console.readLineAsync, Console.print를 활용한다.
 
 class App {
-  play() {
-    Console.print('숫자 야구 게임을 시작합니다.');
-    const computerNumbers = this.generateComputerNumbers();
-    this.gameLoop(computerNumbers);
-  }
+  async play() {
+    let gameEnded = false;
 
-  generateComputerNumbers() {
-    const computerNumbers = [];
-    while (computerNumbers.length < 3) {
-      const number = Random.pickNumberInRange(1, 9);
-      if (!computerNumbers.includes(number)) {
-        computerNumbers.push(number);
-      }
-    }
-    return computerNumbers;
-  }
-
-  gameLoop(computerNumbers) {
-    Console.readLine('숫자를 입력해주세요 : ', (userInput) => {
-      if (!/^\d{3}$/.test(userInput)) {
-        throw new Error('잘못된 입력입니다. 3자리 숫자를 입력해주세요.');
+    while (!gameEnded) {
+      //임의로 정답 지정
+      const computer = []; //const는 선언 후 값 변경 불가, 배열(정답) 지정
+      while (computer.length < 3) {
+        // 3자리수로 각 자리의 수를 중복 없이 설정(1~9)
+        const number = Random.pickNumberInRange(1, 9);
+        if (!computer.includes(number)) {
+          // 연산자로 중복 확인 중복이 없는 경우에만 push함수로 값을 추가하여 중복제외
+          computer.push(number);
+        }
       }
 
-      const userNumbers = [...userInput].map(Number);
-      const result = this.calculateResult(computerNumbers, userNumbers);
-      Console.print(result);
+      // 입력
+      // 세자리 수, 반복문(사이즈축소),
+      // 배열을 입력받아서 각 자리의 인덱스로 비교, 조건을
 
-      if (result === '3스트라이크') {
-        Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-        Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (restart) => {
-          if (restart === '1') {
-            this.play(); // 재시작
-          } else if (restart === '2') {
-            Console.close(); // 종료
-          } else {
-            throw new Error('잘못된 입력입니다.');
+      Console.print('숫자 야구 게임을 시작합니다');
+
+      while (true) {
+        const Guess = await Console.readLineAsync('숫자를 입력해주세요: ');
+        const GuessNum = Guess.split('').map(Number); // 숫자를 입력받고 split()으로 분리, .map(number)로 정수형 숫자배열로 변환
+
+        if (GuessNum.length !== 3 || GuessNum.some((num) => num < 1 || num > 9)) {
+          throw new Error('잘못된 값을 입력했습니다.'); // 배열 길이 3이 아니거나, 숫자가 1~9이외의 값인 경우 throw
+        }
+
+        let ball = 0;
+        let strike = 0;
+
+        for (let i = 0; i < GuessNum.length; i++) {
+          // 볼/스트라이크 기능 구현, 스트라이크가 아님에도 배열에 포함된다면 볼
+          if (GuessNum[i] === computer[i]) {
+            strike++;
+          } else if (computer.includes(GuessNum[i])) {
+            ball++;
           }
-        });
-      } else {
-        // 잘못된 재귀 대신 반복적으로 gameLoop 호출
-        this.gameLoop(computerNumbers);
-      }
-    });
-  }
+        }
 
-  calculateResult(computerNumbers, userNumbers) {
-    let strikes = 0;
-    let balls = 0;
+        // 출력
+        if (strike === 3) {
+          Console.print('3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+          // 재시작 여부 확인
+          const restart = await Console.readLineAsync(' 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
 
-    for (let i = 0; i < 3; i++) {
-      if (userNumbers[i] === computerNumbers[i]) {
-        strikes += 1;
-      } else if (computerNumbers.includes(userNumbers[i])) {
-        balls += 1;
+          if (restart === '2') {
+            Console.print('게임을 종료합니다');
+            return (gameEnded = true);
+          } else if (restart === '1') {
+            gameEnded = false; //바깥 while 루프를 다시 반복
+          }
+          break;
+        } else if (strike === 0 && ball === 0) {
+          Console.print('낫싱');
+        } else {
+          Console.print(`${ball}볼, ${strike}스트라이크`); // ${ball}와 같은 문자열 리터럴을 사용시 백틱(``)으로 감싸야 에러발생 X
+        }
       }
     }
-
-    if (strikes === 0 && balls === 0) {
-      return '낫싱';
-    }
-
-    return `${balls > 0 ? balls + '볼 ' : ''}${strikes > 0 ? strikes + '스트라이크' : ''}`.trim();
   }
 }
 
